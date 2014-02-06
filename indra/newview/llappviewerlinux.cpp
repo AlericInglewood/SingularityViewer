a4c5a8f98d0131d345b8504f47cc9436733c3937
Aleric Inglewood <Aleric.Inglewood@gmail.com>
2014-02-06 18:26:10 +0100

    Remove compiler warning about deprecated g_thread_init().
    From the changelog of glib:
    
    - libgthread is now an empty shell and g_thread_init() is no longer
       required (and has been deprecated)
    
    As of 2.31, g_thread_supported() is a macro defined as (1), so
    that g_thread_init() is never called anyway when compiled against
    this version; however, despite that fact, the compiler still
    produces a warning when parsing the code without this preprocessor
    test.
    
    This means that when the viewer is compiled against glibc 2.31 or
    higher, then it won't run correctly when linked with a lower version
    at runtime; and as long as we want to support older C libraries we
    will have to compile against that older version for releases.
    
    This patch makes no different with regard to that fact (without this
    patch g_thread_init() would also never be called when compiled against
    version 2.31 or higher because g_thread_supported() is a macro
    returning 1, and not a function call). One could call that a bug in
    glibc as it break unnecessarily backwards compatibility, but this is
    out of our hands. It is not a problem however: we already (have to)
    compile on the oldest distribution that we (want to) support.

--- indra/newview/llappviewerlinux.cpp
+++ indra/newview/llappviewerlinux.cpp
@@ -133,12 +133,14 @@ LLAppViewerLinux::~LLAppViewerLinux()
 
 bool LLAppViewerLinux::init()
 {
+#if !GLIB_CHECK_VERSION(2, 31, 0)   // Singu note: This is no longer needed (and has been deprecated) for versions >= 2.31.
 	// g_thread_init() must be called before *any* use of glib, *and*
 	// before any mutexes are held, *and* some of our third-party
 	// libraries likes to use glib functions; in short, do this here
 	// really early in app startup!
 	if (!g_thread_supported ()) g_thread_init (NULL);
-	
+#endif
+
 	bool success = LLAppViewer::init();
 
 #if LL_SEND_CRASH_REPORTS
